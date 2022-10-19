7b041ce18d8967cb6fd52452943d438d414cbb69
Sergy <9058591133@mail.ru>
2022-10-19 15:22:40 +0500

    задачи C#

---
+++ 003/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите число дня недели ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 1)
+{
+    Console.WriteLine("Понедельник ");
+}
+if (number == 2)
+{
+    Console.WriteLine("Вторник ");
+}
+if (number == 3)
+{
+    Console.WriteLine("Среда ");
+}
+if (number == 4)
+{
+    Console.WriteLine("Четверг ");
+}
+if (number == 5)
+{
+    Console.WriteLine("Патница ");
+}
+if (number == 6)
+{
+    Console.WriteLine("Субота ");
+}
+if (number == 7)
+{
+    Console.WriteLine("Воскресение ");
+}
